f67aa85fbece1565da15672160723b3558f74873
delaylee <delaylee219@gmail.com>
2022-07-30 23:22:29 +0900

    :sparkles: add: movies controller
    Use Nest CLI create movies controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController], // url을 가져오고 function을 실행 / like express router
-  providers: [AppService],
+  controllers: [MoviesController], // url을 가져오고 function을 실행 / like express router
+  providers: [],
 }) // class를 위해 움직이는 함수(func)
 export class AppModule {}
