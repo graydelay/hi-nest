7f1952673974b17f5a16fda2e543210142b0158c
delaylee <delaylee219@gmail.com>
2022-08-03 20:59:06 +0900

    :sparkles: update: movie.service.spec.ts
    1. spec.ts 파일에서 단위 테스트 작성
    2. service에 있는 function 하나 당 모두 테스트를 작성하여 converage를 100%로 만듬

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,18 +1,112 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
-  let service: MoviesService;
+  let movieService: MoviesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
     }).compile();
 
-    service = module.get<MoviesService>(MoviesService);
+    movieService = module.get<MoviesService>(MoviesService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(movieService).toBeDefined();
+  });
+
+  describe('getAll()', () => {
+    it('should return an array', () => {
+      const result = movieService.getAll();
+
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne()', () => {
+    it('should return a movie', () => {
+      movieService.create({
+        title: 'Test Move',
+        genres: ['test'],
+        year: 2020,
+      });
+      const movie = movieService.getOne(1);
+
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        movieService.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+  });
+
+  describe('deleteOne()', () => {
+    it('deletes a movie', () => {
+      movieService.create({
+        title: 'Test Move',
+        genres: ['test'],
+        year: 2020,
+      });
+      const beforeDelete = movieService.getAll().length;
+      movieService.deleteOne(1);
+      const afterDelete = movieService.getAll().length;
+
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        movieService.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+  });
+
+  describe('create()', () => {
+    it('should create a movie', () => {
+      const beforeCreate = movieService.getAll().length;
+      movieService.create({
+        title: 'Test Move',
+        genres: ['test'],
+        year: 2020,
+      });
+      const afterCreate = movieService.getAll().length;
+
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update()', () => {
+    it('should update a movie', () => {
+      movieService.create({
+        title: 'Test Move',
+        genres: ['test'],
+        year: 2020,
+      });
+
+      movieService.update(1, { title: 'Updated Test' });
+      const movie = movieService.getOne(1);
+
+      expect(movie.title).toEqual('Updated Test');
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        movieService.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
   });
 });
