2dec652b31b08558bc738efe3aa0d6ff1e869368
delaylee <43333357+graydelay@users.noreply.github.com>
2022-08-04 09:47:18 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   // like middle ware
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // true일 경우 아무 decorator(@)도 없는 어떠한 property의 object를 거른다
+      whitelist: true, // true일 경우 아무 decorator(ex. @Param, @Body, @Query)도 없는 어떠한 property의 object를 거른다
       forbidNonWhitelisted: true, // 리퀘스트 자체를 막아줌
       transform: true, // 유저가 보내는 param을 원하는 실제 타입으로 변경해 줌 (변경없이는 모두 string으로 오게 됨)
     }),
