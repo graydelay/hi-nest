73341840e90178be730d23aefee7610bec767cb8
delaylee <delaylee219@gmail.com>
2022-08-04 22:04:12 +0900

    :sparkles: update: app.e2e-spec.ts
    1. /movies/:id 관련 테스트 작성 (GET, PATCH, DELETE)
    2. 각 테스트 app을 구동할 때는 main 해서 한 것과 동일한 설정을 해줘야 한다
    3. useGlobalPipe, transform을 설정하지 않을 경우 id 값을 transform 하지 못해 오류 발생

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,16 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // true일 경우 아무 decorator(ex. @Param, @Body, @Query)도 없는 어떠한 property의 object를 거른다
+        forbidNonWhitelisted: true, // 리퀘스트 자체를 막아줌
+        transform: true, // 유저가 보내는 param을 원하는 실제 타입으로 변경해 줌 (변경없이는 모두 string으로 오게 됨)
+      }),
+    );
     await app.init();
   });
 
@@ -23,23 +31,67 @@ describe('AppController (e2e)', () => {
   });
 
   describe('/movies', () => {
-    it('GET', () => {
+    it('GET 200', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
           title: 'Test Title',
-          years: 2022,
+          year: 2022,
           genres: ['test'],
         })
         .expect(201);
     });
 
-    it('DELETE', () => {
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test Title',
+          year: 2022,
+          genres: ['test'],
+          other: 'Thing',
+        })
+        .expect(400);
+    });
+
+    it('DELETE 404', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Patch Title' })
+        .expect(200);
+    });
+
+    it('PATCH 404', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/999')
+        .send({ title: 'Patch Title' })
+        .expect(404);
+    });
+
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
+
+    it('DELETE 404', () => {
+      return request(app.getHttpServer()).delete('/movies/999').expect(404);
     });
   });
 });
